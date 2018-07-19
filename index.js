ec4ebc6bce6bcf9a41482c1be55b711ee2c8901b
Tahmid Hasan <web.tahmid@outlook.com>
2018-07-19 10:39:19 +0600

    Add all change

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
-
 const getCustomerSync = require('./lib/get-customer-sync');
 const createJson = require('./lib/create-json');
+const generateSheet = require('./lib/generate-sheet');
 
 const getCustomers = async shop => {
 	let data = await getCustomerSync(shop);
@@ -9,31 +8,6 @@ const getCustomers = async shop => {
 	let dataString = JSON.stringify(data);
 	
 	createJson(dataString);
-};
-
-const generateSheet = (output, cb) => {
-	if(typeof require !== 'undefined') XLSX = require('xlsx');
-
-	const file = fs.readFileSync('./data.json', 'utf-8');
-	const data = JSON.parse(file);
-	const exported = output + '.xlsx';
-	console.log(data.length);
-	const wb = XLSX.utils.book_new();
-
-	wb.Props = {
-		Title: 'Customer-Export',
-		Subject: 'Test File',
-		Author: 'Tahmid Hasan',
-		CreatedDate: new Date()
-	};
-
-	wb.SheetNames.push('Test Sheet');
-	
-	const ws = XLSX.utils.json_to_sheet(data);
-
-	wb.Sheets['Test Sheet'] = ws;
-
-	XLSX.writeFileAsync(exported, wb, {bookType: 'xlsx'}, cb);
 };
 
 module.exports = { getCustomers, generateSheet };
